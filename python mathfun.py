a8bf60907ab28d7edf1e4f1b2ee55f17645c8da2
saikiran335 <saisaikiran335@gmail.com>
2021-04-23 11:39:44 +0530

    math

---
+++ python mathfun.py
@@ -0,0 +1,10 @@
+import math
+
+print(min(10,20,5))
+print(max(10,20,5))
+print(abs(-123.456))
+print(pow(4,3))
+print(int(math.sqrt(625)))
+print(math.ceil(1.4))
+print(math.floor(1.4))
+print(float(math.pi))
