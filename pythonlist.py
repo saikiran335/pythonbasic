35cad85c13665f44a45c2590f3f8e715930e6fe3
saikiran335 <saisaikiran335@gmail.com>
2021-04-26 14:47:32 +0530

    list

---
+++ pythonlist.py
@@ -0,0 +1,12 @@
+list1=["23","24","45","65","11"]
+print(list1)
+print(len(list1))
+print(type(list1))
+print(list1[2:4])
+print(list1[:4])
+print(list1[2:])
+print(list1[:-1])
+print(list1[-4:-1])
+print(list1[::])
+if 45 in list1 :
+    print("yes it is present")
