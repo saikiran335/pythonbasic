27baaf7edf0349b60ea0d06e8320c9b140f6f93e
saikiran335 <saisaikiran335@gmail.com>
2021-04-28 12:39:30 +0530

    dictionaries

---
+++ python dictionaries.py
@@ -0,0 +1,11 @@
+profile={
+    "Name": "Sai",
+    "Age" : 20,
+    "Height" : "6feet"
+    }
+print(profile)
+print(profile["Name"])
+print(profile["Age"])
+print(profile["Height"])
+print(type(profile))
+print(len(profile))
