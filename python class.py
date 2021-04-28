fe589244028ea89ed56584f5e4580f24152152d6
saikiran335 <saisaikiran335@gmail.com>
2021-04-28 15:41:58 +0530

    classes

---
+++ python class.py
@@ -0,0 +1,13 @@
+class person:
+    def __init__(self,name,age,height):
+        self.name=name
+        self.age=age
+        self.height=height
+
+    def profile(a):
+        print("hello my name is:" + a.name)
+        print("my age is:" + a.age)
+        print("and my height is:" + a.height)
+
+p=person("sai","20","6")
+p.profile()
