dd7497b949036b3f57845d34344a54e8acafecd8
saikiran335 <saisaikiran335@gmail.com>
2021-04-28 14:46:07 +0530

    loops

---
+++ python loops.py
@@ -0,0 +1,19 @@
+i=1
+while i<6:
+    print(i)
+    i+=1
+i=1    
+while i<6:
+    print(i)
+    if i==4:
+        break
+    i+=1
+i=1
+while i<6:
+    print(i)
+    if i==4:
+        continue
+    i+=1
+for i in range(1,6):
+    print(i)
+    
