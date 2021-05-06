77a39e23b6880008faad5c125a3920adf385446c
saikiran335 <saisaikiran335@gmail.com>
2021-05-06 12:02:07 +0530

    dictionary example

---
+++ myprofile.py
@@ -0,0 +1,13 @@
+profile={"name":"sai kiran",
+         "surename":"Tirumala",
+         "age":20,
+         "DOB":"27-jun-2000",
+         }
+print(profile)
+print(profile["name"])
+print(profile["surename"])
+print(profile["age"])
+print(profile["DOB"])
+profile["status"]="student"
+print(profile)
+print(profile["status"])
